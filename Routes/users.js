b581902ff5af0fe2101d0795266a694ee82d98c0
Anthony Malary <amalary@bu.edu>
2022-05-16 13:05:55 -0700

    Finish grabbing user functionality

--- Routes/users.js
+++ Routes/users.js
@@ -64,6 +64,18 @@ router.delete("/:id", async(req,res) => {
 
 // Grab a user
 
+router.get("/:id", async (req,res) =>{
+    try{
+        const user = await User.findById(req.params.id);
+        const {password, updatedAt, ...other} = user._doc
+        res.status(200).json(other)
+
+    }catch(err){
+        res.status(500).json(err)
+    }
+
+})
+
 // Follow a user
 
 // Unfollow User
